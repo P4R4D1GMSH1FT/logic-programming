eb2249335f02f026feae7987812a06cd682ee07e
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2024-04-22 18:50:09 +0200

    Update and rename inequality.pl to relation_inequality.pl

---
+++ relation_inequality.pl
@@ -0,0 +1,34 @@
+not_equal(X, Y) :-
+    X \= Y.
+    
+parent(X,Y) :-
+    father(X,Y),
+    mother(X,Y).
+son(X,Y) :-
+    father(Y,X), male(X).
+daughter(X,Y) :-
+    father(Y,X), female(X).
+uncle(Uncle,Person) :-
+    brother(Uncle,Parent), parent(Parent,Person).
+sibling(Sib1,Sib2) :-
+    parent(Parent,Sib1), parent(Parent,Sib2), Sib1 \= Sib2.
+brother(Bro1,Bro2) :-
+    son(mother,father), sibling(mother,father).
+sister(Sis1,Sis2) :-
+    daughter(mother,father), sibling
+cousin(Cousin1,Cousin2) :-
+    parent(Parent1,Cousin1),
+    parent(Parent2,Cousin2),
+    sibling(Parent1,Parent2).
+married_couple(Wife,Husband) :-
+    Wife(X,Y),
+    Husband(X,Y).
+mother_in_law(X,Y) :-
+    mother(Husband,Wife),
+    mother(Wife,Husband).
+father_in_law(X,Y) :-
+    father(Husband,Wife),
+    father(Wife,Husband).
+son_in_law(X,Y)
+    son(Husband,Wife),
+    son(Wife,Husband).
