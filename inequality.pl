abd3daa50d79f4aa65bb73d8bacb9ab518d15499
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2024-04-22 18:10:55 +0200

    Create inequality.pl

---
+++ inequality.pl
@@ -0,0 +1,11 @@
+not_equal(X, Y) :-
+    X \= Y.
+
+uncle(Uncle,Person) :-
+    brother(Uncle,Parent), parent(Parent,Person).
+sibling(Sib1,Sib2) :-
+    parent(Parent,Sib1), parent(Parent,Sib2), Sib1 \= Sib2.
+cousin(Cousin1,Cousin2) :-
+    parent(Parent1,Cousin1),
+    parent(Parent2,Cousin2),
+    sibling(Parent1,Parent2).
