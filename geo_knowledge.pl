dd8e48a96601d4d624b1d2b866d8f15f0bb0c3d4
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2024-04-22 15:15:36 +0200

    Update

---
+++ geo_knowledge.pl
@@ -0,0 +1,12 @@
+located_in(atlanta,georgia).
+located_in(houston,texas).
+located_in(austin,texas).
+located_in(toronto,ontario).
+located_in(warsaw,mazowieckie)
+located_in(X,poland) :- located_in(X,mazowieckie)
+located_in(X,usa) :- located_in(X,georgia).
+located_in(X,usa) :- located_in(X,texas).
+located_in(X,canada) :- located_in(X,ontario).
+located_in(X,north_america) :- located_in(X,usa).
+located_in(X,north_america) :- located_in(X,canada).
+located_in(X,europe) :- located_in(X,poland)
